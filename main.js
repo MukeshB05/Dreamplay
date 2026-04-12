55f5905be0e21bc9ae22c88359f2391f13f7b984
Mukesh <mukesh2305b@gmail.com>
2026-04-12 21:44:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ const channels = [
             { 
                 name: "Big TV 24x7",
                 sources: [
-                    { name: "Server 1", url: "https://anywhere.pwisetthon.com/https://in1.sunilprasad.com.np/yuppLive/big-tv-malayalam/master.m3u8" }
+                    { name: "Server 1", url: "https://d2gvyg6lvauoko.cloudfront.net/230226/bigtvmalyalam/chunks.m3u8" }
                 ], 
                 img: "https://i.ibb.co/hJrJ08X4/Big-TV24x7.jpg", 
                 category: "News",
